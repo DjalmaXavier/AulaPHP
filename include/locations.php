0298e272a03a6e501085f542c94e4330a87aca7c
Djalma Xavier <djalmaxavier42@gmail.com>
2024-06-24 23:35:31 -0300

    add the include lesson

---
+++ include/locations.php
@@ -0,0 +1,22 @@
+<?php
+include("header.html");
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=], initial-scale=1.0">
+    <title>Locations</title>
+</head>
+
+<body>
+    This is locations page
+</body>
+
+</html>
+
+<?php
+include("footer.html");
+?>
